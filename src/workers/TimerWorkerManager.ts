63c17a4f84dbebd4d11a6041ffb688e5f985803a
brunohhomem <brunohhomem@gmail.com>
2025-08-05 11:08:29 -0300

    feat: add timerworker function

--- src/workers/TimerWorkerManager.ts
+++ src/workers/TimerWorkerManager.ts
@@ -1,22 +1,23 @@
+import type { TaskStateModel } from '../models/TaskStateModel'
+
 let instance: TimerWorkerManager | null = null
 
 export class TimerWorkerManager {
   private worker: Worker
 
   private constructor() {
-    this.worker = new Worker(
-      new URL('./workers/timerWorker.js', import.meta.url),
-    )
+    this.worker = new Worker(new URL('./timerWorker.js', import.meta.url))
   }
 
   static getInstance() {
     if (!instance) {
       instance = new TimerWorkerManager()
     }
+
     return instance
   }
 
-  postMessage(message: any) {
+  postMessage(message: TaskStateModel) {
     this.worker.postMessage(message)
   }
 
