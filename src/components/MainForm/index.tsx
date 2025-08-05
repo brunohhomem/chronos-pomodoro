2fec80c4b1e841e8969df5a12f1cb3e266a9f37f
brunohhomem <brunohhomem@gmail.com>
2025-08-05 12:10:25 -0300

    feat: implements toastify

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 import type { TaskModel } from '../../models/TaskModel';
 import { Tips } from '../Tips';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -20,13 +21,14 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss()
 
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert('Digite o nome da tarefa');
+      showMessage.warn('Digite o nome da tarefa');
       return;
     }
 
@@ -41,9 +43,14 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+
+    showMessage.info('Iniciando sua tarefa...');
   }
 
   function handleInterruptTask() {
+    showMessage.dismiss()
+    showMessage.error('Tarefa interrompida...');
+
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
